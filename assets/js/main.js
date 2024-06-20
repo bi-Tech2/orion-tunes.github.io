c0a90a64be683e1408bedd7e4685f7072a606dd1
bi-Tech2 <141980137+bi-Tech2@users.noreply.github.com>
2024-06-20 16:25:08 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,76 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleOverlayButtons = document.querySelectorAll('.toggle-overlay');
+    const playButtons = document.querySelectorAll('.play-btn');
+    const closeButtons = document.querySelectorAll('.close-btn');
+    const marquees = document.querySelectorAll('marquee');
+    
+    toggleOverlayButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const fullLay = button.closest('.j').querySelector('.full-lay');
+            fullLay.style.display = fullLay.style.display === 'none' || !fullLay.style.display ? 'flex' : 'none';
+        });
+    });
+    
+    
+    playButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const icon = button.querySelector('i');
+            const marquee = button.closest('.j').querySelector('marquee');
+            if (icon.classList.contains('bx-play')) {
+                icon.classList.replace('bx-play', 'bx-pause');
+                marquee.start();
+            } else {
+                icon.classList.replace('bx-pause', 'bx-play');
+                marquee.stop();
+            }
+        });
+    });
+
+    closeButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const fullLay = button.closest('.full-lay');
+            fullLay.style.display = 'none';
+        });
+    });
+});
+
+
+
+// JavaScript for swiping functionality
+const carousel = document.getElementById('imageCarousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+    isDown = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+    isDown = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust scrolling speed here
+    carousel.scrollLeft = scrollLeft - walk;
+});
+
+
+
+ 
