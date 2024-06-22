f47f575b74a34626fe90061af370b3a653fa1ffa
bi-Tech2 <141980137+bi-Tech2@users.noreply.github.com>
2024-06-22 18:42:50 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,67 @@
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]');
+document.addEventListener('DOMContentLoaded', function() {
+    // Loading Screen
+    setTimeout(function() {
+        var loadingScreen = document.getElementById('loading-screen');
+        loadingScreen.style.opacity = 0;
 
-function scrollActive() {
-    const scrollY = window.pageYOffset;
+        // Wait for the transition to end before setting display to none
+        setTimeout(function() {
+            loadingScreen.style.display = 'none';
+            document.getElementById('content').style.display = 'block';  // Show the content
+        }, 500); // Match this to the transition duration
+    }, 5000); // Disappear after 3 seconds
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute('id');
+    // Scroll Sections Active Link
+    const sections = document.querySelectorAll('section[id]');
 
-        const link = document.querySelector('.nav__menu a[href*="' + sectionId + '"]');
+    function scrollActive() {
+        const scrollY = window.pageYOffset;
 
-        if (link) {  // Check if the link exists
-            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                link.classList.add('active-link');
-            } else {
-                link.classList.remove('active-link');
+        sections.forEach(current => {
+            const sectionHeight = current.offsetHeight;
+            const sectionTop = current.offsetTop - 50;
+            const sectionId = current.getAttribute('id');
+
+            const link = document.querySelector('.nav__menu a[href*="' + sectionId + '"]');
+
+            if (link) {  // Check if the link exists
+                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                    link.classList.add('active-link');
+                } else {
+                    link.classList.remove('active-link');
+                }
             }
-        }
-    });
-}
-
-window.addEventListener('scroll', scrollActive);
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader() {
-    const header = document.getElementById('header');
-    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if (window.scrollY >= 80) {
-        header.classList.add('scroll-header');
-    } else {
-        header.classList.remove('scroll-header');
+        });
     }
-}
 
-window.addEventListener('scroll', scrollHeader);
+    window.addEventListener('scroll', scrollActive);
 
-document.addEventListener('DOMContentLoaded', () => {
+    // Change Background Header
+    function scrollHeader() {
+        const header = document.getElementById('header');
+        // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+        if (window.scrollY >= 80) {
+            header.classList.add('scroll-header');
+        } else {
+            header.classList.remove('scroll-header');
+        }
+    }
+
+    window.addEventListener('scroll', scrollHeader);
+
+    // Overlay and Marquee Controls
     const toggleOverlayButtons = document.querySelectorAll('.toggle-overlay');
     const playButtons = document.querySelectorAll('.play-btn');
     const closeButtons = document.querySelectorAll('.close-btn');
     const marquees = document.querySelectorAll('marquee');
-    
+
     toggleOverlayButtons.forEach((button, index) => {
         button.addEventListener('click', () => {
             const fullLay = button.closest('.j').querySelector('.full-lay');
             fullLay.style.display = fullLay.style.display === 'none' || !fullLay.style.display ? 'flex' : 'none';
         });
     });
-    
+
     playButtons.forEach((button) => {
         button.addEventListener('click', () => {
             const icon = button.querySelector('i');
@@ -69,253 +82,222 @@ document.addEventListener('DOMContentLoaded', () => {
             fullLay.style.display = 'none';
         });
     });
+
+    // Carousel Swiping
+    const carousel = document.getElementById('imageCarousel');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    if (carousel) {
+        carousel.addEventListener('mousedown', (e) => {
+            isDown = true;
+            startX = e.pageX - carousel.offsetLeft;
+            scrollLeft = carousel.scrollLeft;
+        });
+
+        carousel.addEventListener('mouseleave', () => {
+            isDown = false;
+        });
+
+        carousel.addEventListener('mouseup', () => {
+            isDown = false;
+        });
+
+        carousel.addEventListener('mousemove', (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - carousel.offsetLeft;
+            const walk = (x - startX) * 2; // Adjust scrolling speed here
+            carousel.scrollLeft = scrollLeft - walk;
+        });
+    }
+
+    // Table Features
+    const search = document.querySelector('.input-group input'),
+        table_rows = document.querySelectorAll('tbody tr'),
+        table_headings = document.querySelectorAll('thead th');
+
+    // 1. Searching for specific data of HTML table
+    search.addEventListener('input', searchTable);
+
+    function searchTable() {
+        table_rows.forEach((row, i) => {
+            let table_data = row.textContent.toLowerCase(),
+                search_data = search.value.toLowerCase();
+
+            row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
+            row.style.setProperty('--delay', i / 25 + 's');
+        })
+
+        document.querySelectorAll('tbody tr:not(.hide)').forEach((visible_row, i) => {
+            visible_row.style.backgroundColor = (i % 2 == 0) ? 'transparent' : '#0000000b';
+        });
+    }
+
+    // 2. Sorting | Ordering data of HTML table
+    table_headings.forEach((head, i) => {
+        let sort_asc = true;
+        head.onclick = () => {
+            table_headings.forEach(head => head.classList.remove('active'));
+            head.classList.add('active');
+
+            document.querySelectorAll('td').forEach(td => td.classList.remove('active'));
+            table_rows.forEach(row => {
+                row.querySelectorAll('td')[i].classList.add('active');
+            })
+
+            head.classList.toggle('asc', sort_asc);
+            sort_asc = head.classList.contains('asc') ? false : true;
+
+            sortTable(i, sort_asc);
+        }
+    })
+
+    function sortTable(column, sort_asc) {
+        [...table_rows].sort((a, b) => {
+            let first_row = a.querySelectorAll('td')[column].textContent.toLowerCase(),
+                second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
+
+            return sort_asc ? (first_row < second_row ? 1 : -1) : (first_row < second_row ? -1 : 1);
+        })
+        .map(sorted_row => document.querySelector('tbody').appendChild(sorted_row));
+    }
+
+    // 3. Converting HTML table to PDF
+    const pdf_btn = document.querySelector('#toPDF');
+    const customers_table = document.querySelector('#customers_table');
+
+    const toPDF = function (customers_table) {
+        const html_code = `
+        <!DOCTYPE html>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <main class="table" id="customers_table">${customers_table.innerHTML}</main>`;
+
+        const new_window = window.open();
+         new_window.document.write(html_code);
+
+        setTimeout(() => {
+            new_window.print();
+            new_window.close();
+        }, 400);
+    }
+
+    pdf_btn.onclick = () => {
+        toPDF(customers_table);
+    }
+
+    // 4. Converting HTML table to JSON
+    const json_btn = document.querySelector('#toJSON');
+
+    const toJSON = function (table) {
+        let table_data = [],
+            t_head = [],
+
+            t_headings = table.querySelectorAll('th'),
+            t_rows = table.querySelectorAll('tbody tr');
+
+        for (let t_heading of t_headings) {
+            let actual_head = t_heading.textContent.trim().split(' ');
+
+            t_head.push(actual_head.splice(0, actual_head.length - 1).join(' ').toLowerCase());
+        }
+
+        t_rows.forEach(row => {
+            const row_object = {},
+                t_cells = row.querySelectorAll('td');
+
+            t_cells.forEach((t_cell, cell_index) => {
+                const img = t_cell.querySelector('img');
+                if (img) {
+                    row_object['customer image'] = decodeURIComponent(img.src);
+                }
+                row_object[t_head[cell_index]] = t_cell.textContent.trim();
+            })
+            table_data.push(row_object);
+        })
+
+        return JSON.stringify(table_data, null, 4);
+    }
+
+    json_btn.onclick = () => {
+        const json = toJSON(customers_table);
+        downloadFile(json, 'json')
+    }
+
+    // 5. Converting HTML table to CSV File
+    const csv_btn = document.querySelector('#toCSV');
+
+    const toCSV = function (table) {
+        const t_heads = table.querySelectorAll('th'),
+            tbody_rows = table.querySelectorAll('tbody tr');
+
+        const headings = [...t_heads].map(head => {
+            let actual_head = head.textContent.trim().split(' ');
+            return actual_head.splice(0, actual_head.length - 1).join(' ').toLowerCase();
+        }).join(',') + ',' + 'image name';
+
+        const table_data = [...tbody_rows].map(row => {
+            const cells = row.querySelectorAll('td'),
+                img = decodeURIComponent(row.querySelector('img').src),
+                data_without_img = [...cells].map(cell => cell.textContent.replace(/,/g, ".").trim()).join(',');
+
+            return data_without_img + ',' + img;
+        }).join('\n');
+
+        return headings + '\n' + table_data;
+    }
+
+    csv_btn.onclick = () => {
+        const csv = toCSV(customers_table);
+        downloadFile(csv, 'csv', 'customer orders');
+    }
+
+    // 6. Converting HTML table to EXCEL File
+    const excel_btn = document.querySelector('#toEXCEL');
+
+    const toExcel = function (table) {
+        const t_heads = table.querySelectorAll('th'),
+            tbody_rows = table.querySelectorAll('tbody tr');
+
+        const headings = [...t_heads].map(head => {
+            let actual_head = head.textContent.trim().split(' ');
+            return actual_head.splice(0, actual_head.length - 1).join(' ').toLowerCase();
+        }).join('\t') + '\t' + 'image name';
+
+        const table_data = [...tbody_rows].map(row => {
+            const cells = row.querySelectorAll('td'),
+                img = decodeURIComponent(row.querySelector('img').src),
+                data_without_img = [...cells].map(cell => cell.textContent.trim()).join('\t');
+
+            return data_without_img + '\t' + img;
+        }).join('\n');
+
+        return headings + '\n' + table_data;
+    }
+
+    excel_btn.onclick = () => {
+        const excel = toExcel(customers_table);
+        downloadFile(excel, 'excel');
+    }
+
+    // File Download Function
+    const downloadFile = function (data, fileType, fileName = '') {
+        const a = document.createElement('a');
+        a.download = fileName;
+        const mime_types = {
+            'json': 'application/json',
+            'csv': 'text/csv',
+            'excel': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        }
+        a.href = `data:${mime_types[fileType]};charset=utf-8,${encodeURIComponent(data)}`;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    }
 });
 
-// JavaScript for swiping functionality
-const carousel = document.getElementById('imageCarousel');
-let isDown = false;
-let startX;
-let scrollLeft;
 
-if (carousel) {
-    carousel.addEventListener('mousedown', (e) => {
-        isDown = true;
-        startX = e.pageX - carousel.offsetLeft;
-        scrollLeft = carousel.scrollLeft;
-    });
-
-    carousel.addEventListener('mouseleave', () => {
-        isDown = false;
-    });
-
-    carousel.addEventListener('mouseup', () => {
-        isDown = false;
-    });
-
-    carousel.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - carousel.offsetLeft;
-        const walk = (x - startX) * 2; // Adjust scrolling speed here
-        carousel.scrollLeft = scrollLeft - walk;
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-const search = document.querySelector('.input-group input'),
-    table_rows = document.querySelectorAll('tbody tr'),
-    table_headings = document.querySelectorAll('thead th');
-
-// 1. Searching for specific data of HTML table
-search.addEventListener('input', searchTable);
-
-function searchTable() {
-    table_rows.forEach((row, i) => {
-        let table_data = row.textContent.toLowerCase(),
-            search_data = search.value.toLowerCase();
-
-        row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
-        row.style.setProperty('--delay', i / 25 + 's');
-    })
-
-    document.querySelectorAll('tbody tr:not(.hide)').forEach((visible_row, i) => {
-        visible_row.style.backgroundColor = (i % 2 == 0) ? 'transparent' : '#0000000b';
-    });
-}
-
-// 2. Sorting | Ordering data of HTML table
-
-table_headings.forEach((head, i) => {
-    let sort_asc = true;
-    head.onclick = () => {
-        table_headings.forEach(head => head.classList.remove('active'));
-        head.classList.add('active');
-
-        document.querySelectorAll('td').forEach(td => td.classList.remove('active'));
-        table_rows.forEach(row => {
-            row.querySelectorAll('td')[i].classList.add('active');
-        })
-
-        head.classList.toggle('asc', sort_asc);
-        sort_asc = head.classList.contains('asc') ? false : true;
-
-        sortTable(i, sort_asc);
-    }
-})
-
-
-function sortTable(column, sort_asc) {
-    [...table_rows].sort((a, b) => {
-        let first_row = a.querySelectorAll('td')[column].textContent.toLowerCase(),
-            second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
-
-        return sort_asc ? (first_row < second_row ? 1 : -1) : (first_row < second_row ? -1 : 1);
-    })
-        .map(sorted_row => document.querySelector('tbody').appendChild(sorted_row));
-}
-
-// 3. Converting HTML table to PDF
-
-const pdf_btn = document.querySelector('#toPDF');
-const customers_table = document.querySelector('#customers_table');
-
-
-const toPDF = function (customers_table) {
-    const html_code = `
-    <!DOCTYPE html>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <main class="table" id="customers_table">${customers_table.innerHTML}</main>`;
-
-    const new_window = window.open();
-     new_window.document.write(html_code);
-
-    setTimeout(() => {
-        new_window.print();
-        new_window.close();
-    }, 400);
-}
-
-pdf_btn.onclick = () => {
-    toPDF(customers_table);
-}
-
-// 4. Converting HTML table to JSON
-
-const json_btn = document.querySelector('#toJSON');
-
-const toJSON = function (table) {
-    let table_data = [],
-        t_head = [],
-
-        t_headings = table.querySelectorAll('th'),
-        t_rows = table.querySelectorAll('tbody tr');
-
-    for (let t_heading of t_headings) {
-        let actual_head = t_heading.textContent.trim().split(' ');
-
-        t_head.push(actual_head.splice(0, actual_head.length - 1).join(' ').toLowerCase());
-    }
-
-    t_rows.forEach(row => {
-        const row_object = {},
-            t_cells = row.querySelectorAll('td');
-
-        t_cells.forEach((t_cell, cell_index) => {
-            const img = t_cell.querySelector('img');
-            if (img) {
-                row_object['customer image'] = decodeURIComponent(img.src);
-            }
-            row_object[t_head[cell_index]] = t_cell.textContent.trim();
-        })
-        table_data.push(row_object);
-    })
-
-    return JSON.stringify(table_data, null, 4);
-}
-
-json_btn.onclick = () => {
-    const json = toJSON(customers_table);
-    downloadFile(json, 'json')
-}
-
-// 5. Converting HTML table to CSV File
-
-const csv_btn = document.querySelector('#toCSV');
-
-const toCSV = function (table) {
-    // Code For SIMPLE TABLE
-    // const t_rows = table.querySelectorAll('tr');
-    // return [...t_rows].map(row => {
-    //     const cells = row.querySelectorAll('th, td');
-    //     return [...cells].map(cell => cell.textContent.trim()).join(',');
-    // }).join('\n');
-
-    const t_heads = table.querySelectorAll('th'),
-        tbody_rows = table.querySelectorAll('tbody tr');
-
-    const headings = [...t_heads].map(head => {
-        let actual_head = head.textContent.trim().split(' ');
-        return actual_head.splice(0, actual_head.length - 1).join(' ').toLowerCase();
-    }).join(',') + ',' + 'image name';
-
-    const table_data = [...tbody_rows].map(row => {
-        const cells = row.querySelectorAll('td'),
-            img = decodeURIComponent(row.querySelector('img').src),
-            data_without_img = [...cells].map(cell => cell.textContent.replace(/,/g, ".").trim()).join(',');
-
-        return data_without_img + ',' + img;
-    }).join('\n');
-
-    return headings + '\n' + table_data;
-}
-
-csv_btn.onclick = () => {
-    const csv = toCSV(customers_table);
-    downloadFile(csv, 'csv', 'customer orders');
-}
-
-// 6. Converting HTML table to EXCEL File
-
-const excel_btn = document.querySelector('#toEXCEL');
-
-const toExcel = function (table) {
-    // Code For SIMPLE TABLE
-    // const t_rows = table.querySelectorAll('tr');
-    // return [...t_rows].map(row => {
-    //     const cells = row.querySelectorAll('th, td');
-    //     return [...cells].map(cell => cell.textContent.trim()).join('\t');
-    // }).join('\n');
-
-    const t_heads = table.querySelectorAll('th'),
-        tbody_rows = table.querySelectorAll('tbody tr');
-
-    const headings = [...t_heads].map(head => {
-        let actual_head = head.textContent.trim().split(' ');
-        return actual_head.splice(0, actual_head.length - 1).join(' ').toLowerCase();
-    }).join('\t') + '\t' + 'image name';
-
-    const table_data = [...tbody_rows].map(row => {
-        const cells = row.querySelectorAll('td'),
-            img = decodeURIComponent(row.querySelector('img').src),
-            data_without_img = [...cells].map(cell => cell.textContent.trim()).join('\t');
-
-        return data_without_img + '\t' + img;
-    }).join('\n');
-
-    return headings + '\n' + table_data;
-}
-
-excel_btn.onclick = () => {
-    const excel = toExcel(customers_table);
-    downloadFile(excel, 'excel');
-}
-
-const downloadFile = function (data, fileType, fileName = '') {
-    const a = document.createElement('a');
-    a.download = fileName;
-    const mime_types = {
-        'json': 'application/json',
-        'csv': 'text/csv',
-        'excel': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    }
-    a.href = `
-        data:${mime_types[fileType]};charset=utf-8,${encodeURIComponent(data)}
-    `;
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-}
 
 
 
