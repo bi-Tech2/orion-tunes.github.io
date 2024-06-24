587e67dfdd356072462b918152df3fe37ba26469
bi-Tech2 <141980137+bi-Tech2@users.noreply.github.com>
2024-06-24 22:21:58 +0100

    Add files via upload

--- search.js
+++ search.js
@@ -1,42 +1,34 @@
-const tabsBox = document.querySelector(".tabs-box"),
-allTabs = tabsBox.querySelectorAll(".tab"),
-arrowIcons = document.querySelectorAll(".icon i");
+document.addEventListener('DOMContentLoaded', function() {
+    const tabsBox = document.querySelector('.tabs-box');
+    const arrowIcons = document.querySelectorAll('.icon i');
 
-let isDragging = false;
+    let isDragging = false;
 
-const handleIcons = (scrollVal) => {
-    let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
-    arrowIcons[0].parentElement.style.display = scrollVal <= 0 ? "none" : "flex";
-    arrowIcons[1].parentElement.style.display = maxScrollableWidth - scrollVal <= 1 ? "none" : "flex";
-}
+    const handleIcons = (scrollVal) => {
+        const maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
+        arrowIcons[0].parentElement.style.display = scrollVal > 0 ? 'flex' : 'none';
+        arrowIcons[1].parentElement.style.display = maxScrollableWidth > scrollVal ? 'flex' : 'none';
+    };
 
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        // if clicked icon is left, reduce 350 from tabsBox scrollLeft else add
-        let scrollWidth = tabsBox.scrollLeft += icon.id === "left" ? -340 : 340;
-        handleIcons(scrollWidth);
+    arrowIcons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            let scrollWidth = tabsBox.scrollLeft += icon.id === 'left' ? -340 : 340;
+            handleIcons(tabsBox.scrollLeft);
+        });
     });
-});
 
-allTabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        tabsBox.querySelector(".active").classList.remove("active");
-        tab.classList.add("active");
+    tabsBox.addEventListener('mousedown', () => isDragging = true);
+    tabsBox.addEventListener('mousemove', (e) => {
+        if(!isDragging) return;
+        tabsBox.classList.add('dragging');
+        tabsBox.scrollLeft -= e.movementX;
+        handleIcons(tabsBox.scrollLeft);
     });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+        tabsBox.classList.remove('dragging');
+    });
+
+    tabsBox.addEventListener('scroll', () => handleIcons(tabsBox.scrollLeft));
 });
-
-const dragging = (e) => {
-    if(!isDragging) return;
-    tabsBox.classList.add("dragging");
-    tabsBox.scrollLeft -= e.movementX;
-    handleIcons(tabsBox.scrollLeft)
-}
-
-const dragStop = () => {
-    isDragging = false;
-    tabsBox.classList.remove("dragging");
-}
-
-tabsBox.addEventListener("mousedown", () => isDragging = true);
-tabsBox.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
